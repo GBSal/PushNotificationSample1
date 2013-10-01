a8efae18f1bcfd361dd1eac53483b18368683cd4
M Sal <salman_it24@hotmail.com>
2013-10-02 00:19:15 +0100

    New Sample

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\temp\PushNotificationSample\platforms\android\gen\com\pushapp\R.java \
+ : C:\temp\PushNotificationSample\platforms\android\res\drawable\icon.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-hdpi\icon.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-ldpi\icon.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-mdpi\icon.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-xhdpi\icon.png \
+C:\temp\PushNotificationSample\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\temp\PushNotificationSample\platforms\android\res\layout\main.xml \
+C:\temp\PushNotificationSample\platforms\android\res\values\strings.xml \
+C:\temp\PushNotificationSample\platforms\android\res\xml\config.xml \
+C:\temp\PushNotificationSample\platforms\android\bin\AndroidManifest.xml \
